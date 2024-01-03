5a5fc83154ef584ebbce79f80b568d8914f30c1a
Henrik Paul <henrik.paul@gmail.com>
2024-01-03 19:26:41 +0200

    updare app if version updates

--- packages/web-ui/src/routes/+layout.svelte
+++ packages/web-ui/src/routes/+layout.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+  import { beforeNavigate } from "$app/navigation";
+  import { updated } from "$app/stores";
+
+  beforeNavigate(({ willUnload, to }) => {
+    if ($updated && !willUnload && to?.url) {
+      location.href = to.url.href;
+    }
+  });
+</script>
+
 <div class="root">
   <nav>
     <ol>
