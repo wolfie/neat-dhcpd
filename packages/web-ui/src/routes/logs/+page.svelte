26a32ee6c1a0b0a71d55b72157bb2c6048638e11
Henrik Paul <henrik.paul@gmail.com>
2024-01-03 19:40:10 +0200

    add log polling

--- packages/web-ui/src/routes/logs/+page.svelte
+++ packages/web-ui/src/routes/logs/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import type { PageData } from "./$types";
 
   export let data: PageData;
+
+  let logs: PageData["logs"] | undefined = undefined;
+  onMount(() => {
+    const interval = setInterval(async () => {
+      fetch("/api/logs")
+        .then((response) => response.json())
+        .then((newLogs) => (logs = newLogs));
+    }, 2000);
+    return () => clearInterval(interval);
+  });
 </script>
 
 <h1>Logs</h1>
-
+<!-- TODO add log level filtering-->
 <table>
   <thead>
     <tr>
@@ -15,14 +26,14 @@
     </tr>
   </thead>
   <tbody>
-  {#each data.logs as log}
-    <tr>
-      <td class="timestamp">{log.timestamp}</td>
-      <td>{log.system}</td>
-      <td>{log.json}</td>
-    </tr>
-  {/each}
-</tbody>
+    {#each logs ?? data.logs as log (log.timestamp)}
+      <tr>
+        <td class="timestamp">{log.timestamp}</td>
+        <td>{log.system}</td>
+        <td>{log.json}</td>
+      </tr>
+    {/each}
+  </tbody>
 </table>
 
 <style lang="scss">
